f231dccd846d003a9eb5242f7fb53a59fd600037
Артур Кочесоков <artkoches@gmail.com>
2024-03-21 05:41:58 +0300

    add homeWork 2.10

--- script.js
+++ script.js
@@ -1,12 +1,85 @@
 //homeWork 2.9
-
 //main variables
 const writeComment = document.getElementById("comment-btn");
 const listComment = document.getElementById("comments-list");
 const typeUserName = document.getElementById("user-name");
 const typeUserComment = document.getElementById("user-comment");
-const likeNumbers = 0;
 //
+
+//start homeWork 2.10 !!!
+const users = [
+  {
+    name: "Глеб Фокин",
+    time: "12.02.22 12:18",
+    comment: "Это будет первый комментарий на этой странице",
+    numberOfLikes: 3,
+    isLiked: false,
+  },
+  {
+    name: "Варвара Н.",
+    time: "13.02.22 19:22",
+    comment: "Мне нравится как оформлена эта страница! ❤",
+    numberOfLikes: 75,
+    isLiked: false,
+  },
+];
+
+const initLikeBtn = () => {
+  const likeBtn = document.querySelectorAll(".like-button");
+
+  likeBtn.forEach((like) => {
+    like.addEventListener("click", (event) => {
+      // event.target.classList.toggle("-active-like");
+
+      const index = like.dataset.index;
+
+      if (!users[index].isLiked) {
+        users[index].isLiked = true;
+        users[index].numberOfLikes++;
+      } else {
+        users[index].isLiked = false;
+        users[index].numberOfLikes--;
+      }
+
+      renderUsers();
+    });
+  });
+};
+
+const renderUsers = () => {
+  const usersHtml = users
+    .map((user, index) => {
+      return `<li class="comment">
+    <div class="comment-header">
+    <div>${user.name}</div>
+    <div>${user.time}</div>
+    </div>
+    <div class="comment-body">
+    <div class="comment-text" data-index>
+    ${user.comment}
+    </div>
+    </div>
+    <div class="comment-footer">
+    <div class="likes">
+    <span class="likes-counter">${user.numberOfLikes}</span>
+    <button class="like-button ${
+      user.isLiked ? "-active-like" : ""
+    }" data-index="${index}"></button>
+          </div>
+          </div>
+          </li>`;
+    })
+    .join("");
+
+  listComment.innerHTML = usersHtml;
+
+  initLikeBtn();
+  addCommentByKey();
+  resetInputType();
+};
+
+renderUsers();
+//finish homeWork 2.10 !!!
 
 //main comment add function
 function addComment() {
@@ -14,29 +87,15 @@ function addComment() {
     return;
   }
 
-  const oldCommentList = listComment.innerHTML;
+  users.push({
+    name: typeUserName.value,
+    time: getCommentDate(),
+    comment: typeUserComment.value,
+    numberOfLikes: 0,
+    isLiked: false,
+  });
 
-  listComment.innerHTML =
-    oldCommentList +
-    `<li class="comment">
-      <div class="comment-header">
-            <div>${typeUserName.value}</div>
-            <div>${getCommentDate()}</div>
-            </div>
-            <div class="comment-body">
-            <div class="comment-text">
-            ${typeUserComment.value}
-            </div>
-            </div>
-            <div class="comment-footer">
-            <div class="likes">
-            <span class="likes-counter">${likeNumbers}</span>
-            <button class="like-button"></button>
-            </div>
-            </div>
-            </li>`;
-
-  resetInputType();
+  renderUsers();
 }
 
 writeComment.addEventListener("click", () => addComment());
@@ -70,7 +129,6 @@ function getCommentDate() {
 //
 
 //additional tasks!
-
 //task 1
 const activeOrInactiveBtn = () => {
   typeUserName.value.trim() && typeUserComment.value.trim()
@@ -90,8 +148,6 @@ function addCommentByKey() {
     }
   });
 }
-
-addCommentByKey();
 //
 
 //task 3
@@ -99,7 +155,8 @@ function delLastComment() {
   const deleteComment = document.getElementById("del-comment-btn");
 
   deleteComment.addEventListener("click", () => {
-    listComment.removeChild(listComment.lastElementChild);
+    const index = listComment.innerHTML.lastIndexOf('<li class="comment">');
+    listComment.innerHTML = listComment.innerHTML.slice(0, index);
   });
 }
 
