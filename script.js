651e2bd0aae5389038ae9ca4db4c80e9a6e46e51
Артур Кочесоков <artkoches@gmail.com>
2024-03-31 01:28:12 +0300

    3. Сделал рефакторинг кода и теперь намного удобнее ориентироваться в нем.

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
 "use strict";
 
-//homeWork 2.12
-//API method "GET" function
+//variables
+const writeComment = document.getElementById("comment-btn");
+const listComment = document.getElementById("comments-list");
+const typeUserName = document.getElementById("user-name");
+const typeUserComment = document.getElementById("user-comment");
+
+let users = [];
+//
+
+//render function
+const renderUsers = () => {
+  const usersHtml = users
+    .map((user, index) => {
+      const likeBtnClass = user.isLiked ? "-active-like" : "";
+
+      return `<li class="comment">
+    <div class="comment-header">
+    <div>${user.name}</div>
+    <div>${user.date}</div>
+    </div>
+    <div class="comment-body">
+    <div class="comment-text" data-index="${index}">
+    ${user.text}
+    </div>
+    </div>
+    <div class="comment-footer">
+    <button class="redact-comment-btn" data-index="${index}"></button>
+    <div class="likes">
+    <span class="likes-counter">${user.likes}</span>
+    <button class="like-button ${likeBtnClass}" data-index="${index}"></button>
+          </div>
+          </div>
+          </li>`;
+    })
+    .join("");
+
+  listComment.innerHTML = usersHtml;
+
+  replyComment();
+  initLikeBtn();
+  addCommentByKey();
+};
+renderUsers();
+//
+
+//API 'GET' method function
 function getUserComments() {
   const fetchPromise = fetch(
     "https://wedev-api.sky.pro/api/v1/:Artur-Kochesokov/comments",
@@ -44,63 +88,10 @@ function getUserComments() {
     });
   });
 }
-
 getUserComments();
 //
 
-//homeWork 2.9
-//main variables
-const writeComment = document.getElementById("comment-btn");
-const listComment = document.getElementById("comments-list");
-const typeUserName = document.getElementById("user-name");
-const typeUserComment = document.getElementById("user-comment");
-//
-
-//start homeWork 2.10
-//main users list in array
-let users = [];
-//
-
-//js to html render main function
-const renderUsers = () => {
-  const usersHtml = users
-    .map((user, index) => {
-      const likeBtnClass = user.isLiked ? "-active-like" : "";
-
-      return `<li class="comment">
-    <div class="comment-header">
-    <div>${user.name}</div>
-    <div>${user.date}</div>
-    </div>
-    <div class="comment-body">
-    <div class="comment-text" data-index="${index}">
-    ${user.text}
-    </div>
-    </div>
-    <div class="comment-footer">
-    <button class="redact-comment-btn" data-index="${index}"></button>
-    <div class="likes">
-    <span class="likes-counter">${user.likes}</span>
-    <button class="like-button ${likeBtnClass}" data-index="${index}"></button>
-          </div>
-          </div>
-          </li>`;
-    })
-    .join("");
-
-  listComment.innerHTML = usersHtml;
-
-  replyComment();
-  initLikeBtn();
-  addCommentByKey();
-  initRedactBtn();
-};
-
-renderUsers();
-//
-//finish homeWork 2.10
-
-//main comment add function
+//add comment
 function addComment() {
   if (!typeUserName.value.trim() || !typeUserComment.value.trim()) {
     return;
@@ -132,7 +123,7 @@ function addComment() {
 writeComment.addEventListener("click", () => addComment());
 //
 
-//function add event listener for like/dislike buttons
+//init like/dislike button
 function initLikeBtn() {
   const likeBtn = document.querySelectorAll(".like-button");
 
@@ -153,21 +144,7 @@ function initLikeBtn() {
 }
 //
 
-//start homeWork 2.11
-//reply comment function
-function replyComment() {
-  const comment = document.querySelectorAll(".comment-text");
-
-  comment.forEach((el) => {
-    el.addEventListener("click", () => {
-      const index = el.dataset.index;
-      typeUserComment.value = `QUOTE_BEGIN ${users[index].name}: \n ${users[index].text} QUOTE_END`;
-    });
-  });
-}
-//
-
-//safe input function
+//safe input
 function safeInput(str) {
   return str
     .replaceAll("&", "&amp;")
@@ -176,9 +153,8 @@ function safeInput(str) {
     .replaceAll('"', "&quot;");
 }
 //
-//finish homeWork 2.11
 
-//reset for input type function
+//reset for input type
 function resetInputType() {
   (typeUserName.value = ""),
     (typeUserComment.value = ""),
@@ -186,27 +162,7 @@ function resetInputType() {
 }
 //
 
-//comment date & time function
-function getCommentDate() {
-  const dateOptions = {
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  };
-
-  const commentTime = new Date()
-    .toLocaleDateString("ru-RU", dateOptions)
-    .split(",")
-    .join("");
-
-  return commentTime;
-}
-//
-
-//additional tasks!
-//task 1
+//extended input validate
 const activeOrInactiveBtn = () => {
   typeUserName.value.trim() && typeUserComment.value.trim()
     ? (writeComment.disabled = false)
@@ -217,7 +173,7 @@ typeUserName.addEventListener("input", () => activeOrInactiveBtn());
 typeUserComment.addEventListener("input", () => activeOrInactiveBtn());
 //
 
-//task 2
+//add comment by 'Enter' key
 function addCommentByKey() {
   document.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
@@ -227,7 +183,7 @@ function addCommentByKey() {
 }
 //
 
-//task 3 (fixed after add js to html render function)
+//delete last comment
 function delLastComment() {
   const deleteComment = document.getElementById("del-comment-btn");
 
@@ -236,13 +192,11 @@ function delLastComment() {
     renderUsers();
   });
 }
-
 delLastComment();
 //
 
-//task 4
-//function add event listener for redact comments buttons
-function initRedactBtn() {
+//reply comment
+function replyComment() {
   const redactBtn = document.querySelectorAll(".redact-comment-btn");
 
   redactBtn.forEach((button) => {
@@ -252,5 +206,6 @@ function initRedactBtn() {
     });
   });
 }
+//
 
 console.log("It works!");
